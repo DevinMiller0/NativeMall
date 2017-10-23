68a15e4b7ad737609618506a591dfa662d289f87
xwangdesheng <879688122@qq.com>
2017-10-23 23:10:35 +0800

    。

---
+++ app/src/main/java/com/pamo/nativemall/activity/CreatedCardActivity.java
@@ -0,0 +1,26 @@
+package com.pamo.nativemall.activity;
+
+import android.view.View;
+
+import com.pamo.nativemall.R;
+
+/**
+ * Created by 德胜 on 2017/10/23.
+ */
+
+public class CreatedCardActivity extends BaseActivity {
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_created_card;
+    }
+
+    @Override
+    protected void setLayout() {
+
+    }
+
+    @Override
+    protected void onViewClick(View view) {
+
+    }
+}
--- app/src/main/java/com/pamo/nativemall/fragment/MyCardFragment.java
+++ app/src/main/java/com/pamo/nativemall/fragment/MyCardFragment.java
@@ -1,14 +1,22 @@
 package com.pamo.nativemall.fragment;
 
+import android.content.Intent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.pamo.nativemall.R;
+import com.pamo.nativemall.activity.CreatedCardActivity;
 
 /**
  * Created by wangdesheng on 2017/10/23 0023.
  */
 
 public class MyCardFragment extends BaseFragment {
+
+    private TextView creatBtn;
+
     @Override
     protected int getLayout() {
         return R.layout.fragment_my_card;
@@ -16,7 +24,29 @@ public class MyCardFragment extends BaseFragment {
 
     @Override
     protected void setLayout() {
+        initView();
+    }
 
+    private void initView() {
+        creatBtn = getActivity().findViewById(R.id.tv_created);
+        creatBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                    case MotionEvent.ACTION_UP:{
+                        creatBtn.setBackgroundResource(R.drawable.bg_creat_card);
+                        Intent intent = new Intent(getContext(), CreatedCardActivity.class);
+                        startActivity(intent);
+                        break;
+                    }
+                    case MotionEvent.ACTION_DOWN:{
+                        creatBtn.setBackgroundResource(R.drawable.bg_created_click);
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
     }
 
     @Override
