a3586ee0c87d58abb992fb34e79e6af5f0162173
xwangdesheng <879688122@qq.com>
2017-10-25 23:06:42 +0800

    ygbj

--- app/src/main/java/com/pamo/nativemall/fragment/HomeFragment.java
+++ app/src/main/java/com/pamo/nativemall/fragment/HomeFragment.java
@@ -23,22 +23,9 @@ public class HomeFragment extends BaseFragment {
 
     @Override
     protected void setLayout() {
-        share = getActivity().findViewById(R.id.btn_share);
-        share.setOnClickListener(this);
     }
 
     @Override
     protected void onViewClick(View view) {
-
-        switch (view.getId()){
-            case R.id.btn_share:{
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_SEND);
-                intent.putExtra(Intent.EXTRA_PROCESS_TEXT, "THERE IS A TEXT OF SHARE");
-                intent.setType("text/plain");
-                startActivity(Intent.createChooser(intent, "分享到..."));
-                break;
-            }
-        }
     }
 }
