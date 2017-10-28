5edba1e5ae89332f2ed23150a96ee089e357b540
xwangdesheng <879688122@qq.com>
2017-10-28 23:29:18 +0800

    add modify business card function

--- app/src/main/java/com/pamo/nativemall/activity/MainActivity.java
+++ app/src/main/java/com/pamo/nativemall/activity/MainActivity.java
@@ -20,7 +20,6 @@ import com.pamo.nativemall.fragment.SortFragment;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FrameLayout fragmentContent;
-    private RadioButton home;
     private RadioButton sort;
     private RadioButton navigation;
     private RadioButton mine;
@@ -40,17 +39,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void initView() {
 
         fragmentContent = (FrameLayout) findViewById(R.id.fragment_content);
-        home = (RadioButton) findViewById(R.id.rb_home);
         sort = (RadioButton) findViewById(R.id.rb_sort);
         navigation = (RadioButton) findViewById(R.id.rb_navigation);
         mine = (RadioButton) findViewById(R.id.rb_mine);
 
-        home.setOnClickListener(this);
         sort.setOnClickListener(this);
         navigation.setOnClickListener(this);
         mine.setOnClickListener(this);
 
-        setIcon(home, R.mipmap.home_selected, R.color.colorRadioSelect);
+        setIcon(sort, R.mipmap.btn_classify_choosen);
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
         ft.replace(R.id.fragment_content, new HomeFragment());
@@ -62,47 +59,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
         switch (view.getId()){
-            case R.id.rb_home:{
-                ft.replace(R.id.fragment_content, new HomeFragment());
-                setIcon(home, R.mipmap.home_selected, R.color.colorRadioSelect);
-                setIcon(sort, R.mipmap.sort, R.color.colorRadioButton);
-                setIcon(navigation, R.mipmap.navigation, R.color.colorRadioButton);
-                setIcon(mine, R.mipmap.mine, R.color.colorRadioButton);
-                break;
-            }
+
             case R.id.rb_sort:{
                 ft.replace(R.id.fragment_content, new SortFragment());
-                setIcon(home, R.mipmap.home, R.color.colorRadioButton);
-                setIcon(sort, R.mipmap.sort_select, R.color.colorRadioSelect);
-                setIcon(navigation, R.mipmap.navigation, R.color.colorRadioButton);
-                setIcon(mine, R.mipmap.mine, R.color.colorRadioButton);
+                setIcon(sort, R.mipmap.btn_classify_choosen);
+                setIcon(navigation, R.mipmap.btn_nav_normal1);
+                setIcon(mine, R.mipmap.btn_person_normal);
                 break;
             }
             case R.id.rb_navigation:{
                 ft.replace(R.id.fragment_content, new NavigationFragment());
-                setIcon(home, R.mipmap.home, R.color.colorRadioButton);
-                setIcon(sort, R.mipmap.sort, R.color.colorRadioButton);
-                setIcon(navigation, R.mipmap.navigation_selected, R.color.colorRadioSelect);
-                setIcon(mine, R.mipmap.mine, R.color.colorRadioButton);
+                setIcon(sort, R.mipmap.btn_classify_normal);
+                setIcon(navigation, R.mipmap.btn_nav_choosen);
+                setIcon(mine, R.mipmap.btn_person_normal);
                 break;
             }
             case R.id.rb_mine:{
                 ft.replace(R.id.fragment_content, new MineFragment());
-                setIcon(home, R.mipmap.home, R.color.colorRadioButton);
-                setIcon(sort, R.mipmap.sort, R.color.colorRadioButton);
-                setIcon(navigation, R.mipmap.navigation, R.color.colorRadioButton);
-                setIcon(mine, R.mipmap.mine_selected, R.color.colorRadioSelect);
+                setIcon(sort, R.mipmap.btn_classify_normal);
+                setIcon(navigation, R.mipmap.btn_nav_normal1);
+                setIcon(mine, R.mipmap.btn_person_choosen);
                 break;
             }
         }
         ft.commit();
     }
 
-    private void setIcon(RadioButton radioButton, int icon, int color){
+    private void setIcon(RadioButton radioButton, int icon){
         Drawable drawable = getResources().getDrawable(icon);
         drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());
         radioButton.setCompoundDrawables(null, drawable, null, null);
-        radioButton.setTextColor(getResources().getColor(color));
     }
 
     /**
